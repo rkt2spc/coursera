3cc157201a1883ecb36d3b98144bd03d6b13c880
Tuan Nguyen <nmtuan.dev@gmail.com>
2020-12-10 01:06:44 +0800

    week3 session2 quiz1

--- princeton-algorithms-part1/week3/2-quick-sort/quiz/question1/NutsAndBolts.java
+++ princeton-algorithms-part1/week3/2-quick-sort/quiz/question1/NutsAndBolts.java
@@ -4,6 +4,14 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.AbstractMap.SimpleImmutableEntry;
 
+// A disorganized carpenter has a mixed pile of n nuts and n bolts.
+// The goal is to find the corresponding pairs of nuts and bolts.
+// Each nut fits exactly one bolt and each bolt fits exactly one nut.
+// By fitting a nut and a bolt together, the carpenter can see which one is
+// bigger (but the carpenter cannot compare two nuts or two bolts directly).
+// Design an algorithm for the problem that uses at most proportional to
+// n.log(n) compares (probabilistically).
+
 public class NutsAndBolts {
   // Make sure you can't compare a Nut to a Nut, a Bolt to a Bolt
   // But you can compare a Nut and a Bolt
