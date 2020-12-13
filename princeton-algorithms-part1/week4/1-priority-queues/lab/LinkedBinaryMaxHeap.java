69d1e9d072198d9a78e39e28b85e92d38e679292
Tuan Nguyen <nmtuan.dev@gmail.com>
2020-12-14 00:38:42 +0800

    refactor

--- princeton-algorithms-part1/week4/1-priority-queues/lab/LinkedBinaryMaxHeap.java
+++ princeton-algorithms-part1/week4/1-priority-queues/lab/LinkedBinaryMaxHeap.java
@@ -1,7 +1,15 @@
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class LinkedBinaryMaxHeap<T extends Comparable<T>> extends LinkedBinaryHeap<T> {
   public LinkedBinaryMaxHeap() {
     super(Comparator.<T>reverseOrder());
+  }
+
+  public static void main(String[] args) {
+    LinkedBinaryHeap<Integer> h = new LinkedBinaryMaxHeap<Integer>();
+    Arrays.asList(10, 4, 15, 20, 0, 30, 2, 4, -1, -3).forEach((n) -> h.push(n));
+    h.pop();
+    System.out.println(h.visualize());
   }
 }
